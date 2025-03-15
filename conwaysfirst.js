d909e429b2048b3269b3e5b79c156e9cf19c6170
Katlyn Stoll <katlynstoll@gmail.com>
2025-03-15 14:30:23 -0400

    made the program work

--- conwaysfirst.js
+++ conwaysfirst.js
@@ -1,37 +1,56 @@
+/**
+ * For a space that is populated:
+Each cell with one or no neighbors dies, as if by solitude.
 
+
+Each cell with four or more neighbors dies, as if by overpopulation.
+
+
+Each cell with two or three neighbors survives.
+
+
+For a space that is empty or unpopulated:
+Each cell with three neighbors becomes bring to life.
+
+ * 
+ */
 const grid = [
     [true, false,false],
     [false,false,false],
     [false,false,false]
 ];
 
-const result = [];
+const current = grid[0][0];
+const neighbors = [];
 
-function getNeighbors(x,y) {
-    const neighbors = [];
+for (let y = 0; y < 3; y++) {
+    for(let x = 0; x < 3; x++){
+       if(x === 0 && y === 1 ) {
+        neighbors.push(grid[y][x]);
+       } else if(x === 1 && y === 0) {
+        neighbors.push(grid[y][x]);
+       } else if ( x === 1 && y === 1) {
+        neighbors.push(grid[y][x]);
 
-    for (let j = 0; j < 3; j++) {
-        for(let i = 0; i < 3; i++){
-    
-            if(i===x && j===y){
-    
-            } else if(x===1 && y ===0){
-                
-            }
-    
-            result.push(grid[y][x]);
-        }
+       }
+       
+        
     }
-    
 }
 
-/*const trueValues = neighbors.filter(Boolean).length;
-console.log(trueValues); */
-
-const trueValues = neighbors.filter(value => value === true);
-console.log(trueValues);
-console.log(trueValues.length); 
+const aliveNeighbors = neighbors.filter(value => value);
+    
 
 
+console.log(aliveNeighbors.length);
 
-console.log(result);
+
+if(aliveNeighbors.length === 2 || aliveNeighbors.lenght === 3) {
+    console.log("survived!");
+} else if (!current && aliveNeighbors.length === 3){
+    console.log("Bring to Life");
+} else if(current && (aliveNeighbors.length === 0 || aliveNeighbors.length === 1)){
+    console.log("died of lonliness");
+} else {
+    console.log("Oops");
+}
